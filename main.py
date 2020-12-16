227b0c14193c40511b1f0251fc40780a3e3e0074
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 19:04:37 +0000

    Added answers

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ eng = np.loadtxt("energies")[:,1]
 
 # Create a list with 10 elements that you will use to hold the variances
 variances = 10*[0]
-
+eng2 = eng*eng
 # Your code goes here
+for i in range(10) :
+    mean = sum( eng[100*i:100*(i+1)] ) / 100 
+    mean2 = sum( eng2[100*i:100*(i+1)] ) / 100 
+    variances[i] = (100/99)*( mean2 - mean*mean )
 
+mean = sum( eng ) / len(eng)
+mean2 = sum( eng2 ) / len(eng)
+total_var = (len(eng)/(len(eng)-1))*( mean2 - mean*mean )
 # This will draw a graph of your variances
 x = np.linspace( 1, 11, 10 )
 plt.plot( x, variances, 'ko')
