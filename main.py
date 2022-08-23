980089e37f3dbdc7694de7e197c9e4359439e3ea
Gareth Tribello <gareth.tribello@gmail.com>
2022-08-23 15:21:22 +0100

    Fixed tests to use AutoFeedback

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ import numpy as np
 
 # Read in the energies from a file
 eng = np.loadtxt("energies")[:,1]
-
 # Create a list with 10 elements that you will use to hold the variances
-variances = 10*[0]
-
+variances = np.zeros(10)
 # Your code goes here
 
-# This will draw a graph of your variances
-x = np.linspace( 1, 11, 10 )
-plt.plot( x, variances, 'ko')
-plt.plot( [1,10], [total_var,total_var], 'b-' )
-plt.savefig( "block_variances.png")
